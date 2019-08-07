463935037e1cd3e07965afd3266e14b6cfcdba43
Jinbo <33092119+jinbooooom@users.noreply.github.com>
2019-08-07 20:35:19 +0800

    Update selectSort.py

--- Data structure/sort/selectSort.py
+++ Data structure/sort/selectSort.py
@@ -1,17 +1,23 @@
 # -*- coding:utf-8 -*-
 import random
 
+
 class Sort:
-    def selectSort(self, l):  # python 风格
-        if not l:
-            return l
-        m = min(l)
-        l.remove(m)
-        l.insert(0, m)
-        return l[:1] + self.selectSort(l[1:])
+    def selectSort(self, arr):  # python 风格
+        for i in range(len(arr)):
+            m = i
+            for j in range(i + 1, len(arr)):
+                if arr[m] > arr[j]:
+                    m = j
+            if i != m:
+                arr[i], arr[m] = arr[m], arr[i]
+            print(arr)
+        return arr
+
 
 if __name__ == "__main__":
     f = Sort()
-    l = [random.randint(0, 99) for _ in range(10)]
-    print(l)
-    print(f.selectSort(l))
+    arr = [random.randint(0, 99) for _ in range(10)]
+    # arr = [3, 4, 71, 7, 9, 8, 5, 6, 2]
+    print("原始数组：{}\n排序过程：".format(arr))
+    f.selectSort(arr)
