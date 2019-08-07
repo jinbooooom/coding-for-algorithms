bffa0c0069c3fd673dedac131a6f0ae34d0701d8
Jinbo <33092119+jinbooooom@users.noreply.github.com>
2019-08-07 20:48:17 +0800

    Update selectSort.py

--- Data structure/sort/selectSort.py
+++ Data structure/sort/selectSort.py
@@ -2,16 +2,35 @@
 import random
 
 class Sort:
-    def selectSort(self, l):  # python 风格
-        if not l:
-            return l
-        m = min(l)
-        l.remove(m)
-        l.insert(0, m)
-        return l[:1] + self.selectSort(l[1:])
+
+    def selectSort(self, arr):
+        for i in range(len(arr)):
+            m = i
+            for j in range(i + 1, len(arr)):
+                if arr[m] > arr[j]:
+                    m = j
+            if i != m:
+                arr[i], arr[m] = arr[m], arr[i]
+            print(arr)
+        return arr
+
+    def selectSort2(self, arr):  # python 风格的递归实现
+        if not arr:
+            return arr
+        m = min(arr)
+        arr.remove(m)
+        arr.insert(0, m)
+        return arr[:1] + self.selectSort(arr[1:])
+
 
 if __name__ == "__main__":
     f = Sort()
-    l = [random.randint(0, 99) for _ in range(10)]
-    print(l)
-    print(f.selectSort(l))
+    # arr = [random.randint(0, 99) for _ in range(10)]
+    arr = [3, 5, 71, 7, 9, 8, 5, 6, 2]
+    print("原始数组：{}\n排序过程：".format(arr))
+    f.selectSort(arr)
+
+    print("python 风格的递归实现\n")
+    arr = [3, 5, 71, 7, 9, 8, 5, 6, 2]
+    print("原始数组：{}\n排序过程：".format(arr))
+    f.selectSort2(arr)
