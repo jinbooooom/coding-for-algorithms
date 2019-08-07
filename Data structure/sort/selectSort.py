87fe9b6d36db68b63a68fe24806c6e9c199de6d5
Jinbo <33092119+jinbooooom@users.noreply.github.com>
2019-08-07 20:51:04 +0800

    Update selectSort.py

--- Data structure/sort/selectSort.py
+++ Data structure/sort/selectSort.py
@@ -20,7 +20,7 @@ class Sort:
         m = min(arr)
         arr.remove(m)
         arr.insert(0, m)
-        return arr[:1] + self.selectSort(arr[1:])
+        return arr[:1] + self.selectSort2(arr[1:])
 
 
 if __name__ == "__main__":
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     print("原始数组：{}\n排序过程：".format(arr))
     f.selectSort(arr)
 
-    print("python 风格的递归实现\n")
+    print("\npython 风格的递归实现")
     arr = [3, 5, 71, 7, 9, 8, 5, 6, 2]
     print("原始数组：{}\n排序过程：".format(arr))
-    f.selectSort2(arr)
+    print(f.selectSort2(arr))
