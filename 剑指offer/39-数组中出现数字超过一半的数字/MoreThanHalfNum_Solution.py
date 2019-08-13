968d3e1e243fd4a2059f6aaccb7cc6ce4c16ba23
Jinbo <33092119+jinbooooom@users.noreply.github.com>
2019-08-14 00:32:01 +0800

    Update MoreThanHalfNum_Solution.py

--- 剑指offer/39-数组中出现数字超过一半的数字/MoreThanHalfNum_Solution.py
+++ 剑指offer/39-数组中出现数字超过一半的数字/MoreThanHalfNum_Solution.py
@@ -39,16 +39,37 @@ class Solution:
 
     def MoreThanHalfNum_Solution3(self, numbers):
         """
-        仿照快排里的 partition，找任意第 k 大的数字。
+        仿照快排里的 partition，找任意第 k 小的数字。
         数组中出现次数超过一半的数字，必然是中位数，然后找中位数就好了。
         """
         # write code here
-        pass
+        mkey = self.kthmin(numbers, len(numbers)//2)
+        return mkey if numbers.count(mkey) > len(numbers) // 2 else 0
+
+    def kthmin(self, arr, k):
+        """
+        在数组 arr 中找第 k 小数，k = 0 为找最小的数字
+        """
+        key = arr[0]
+        arrLeft = [x for x in arr if x < key] + [x for x in arr if x == key][:-1]
+        arrRight = [x for x in arr if x > key]
+        indexKey = len(arrLeft)
+        # print(arrLeft, key, arrRight)
+        if k == indexKey:
+            # print("k, indexKey, key", k, indexKey, key)
+            return key
+        elif k > indexKey:
+            # print(arrRight, k - indexKey - 1)
+            return self.kthmin(arrRight, k - indexKey - 1)
+        else:
+            # print(arrLeft[:-1], k)
+            return self.kthmin(arrLeft, k)
 
 
 if __name__ == "__main__":
-    num = [1, 2, 3, 2, 2, 2, 5, 4, 2]
+    num = [1, 3, 4, 5, 2, 2, 2, 2, 2]
     f = Solution()
-    print(f.MoreThanHalfNum_Solution2(num))
+    #print(f.MoreThanHalfNum_Solution3(num))
+    print(f.kthmin(num, len(num)//2))
 
 
