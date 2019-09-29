2faa1220b87f36995449aa0a90517388ef266d71
Jinbo <33092119+jinbooooom@users.noreply.github.com>
2019-09-29 14:11:55 +0800

    leetcode136: singleNumber

--- LeetCode/136-出现一次的数字/main.cpp
+++ LeetCode/136-出现一次的数字/main.cpp
@@ -1,18 +1,25 @@
 ﻿
 #include <iostream>
+#include <vector>
+using namespace std;
 
-int singleNumber(int* nums, int numsSize) {
-	int result = 0;
-	for (int i = 0; i < numsSize; ++i) {
-		result ^= nums[i];
+class Solution {
+public:
+	int singleNumber(vector<int>& nums) {
+		int result = 0;
+		for (int i = 0; i < nums.size(); ++i) {
+			result ^= nums[i];
+		}
+		return result;
 	}
-	return result;
-}
+};
 
 int main()
 {
+	Solution method;
 	int arr[10] = {1, 2, 2, 4, 6, 1, 5, 4, 6, 0};
-	std::cout << singleNumber(arr, 10);
+	vector<int> vec(arr, arr + 10);
+	std::cout << method.singleNumber(vec);
 }
 
 
