7cff12eb6558ab7f090824a9c82c48b24cbe0067
Jinbo <jinboom@foxmail.com>
2020-08-07 14:00:01 +0800

    update linkedList.cpp

--- dataStructure/list/linkedList.cpp
+++ dataStructure/list/linkedList.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -9,7 +10,12 @@ using namespace std;
 头节点中不存放数据，只存放指向首节点的指针，
 设置头节点的目的是为了方便对链表的操作，如果不设置头节点，而是直接由头指针指向首节点，
 这样在对头指针后的节点进行插入删除操作时就会与其他节点进行该操作时有所不同，便要作为一种特殊情况来分析
+
+一些注意事项：
+索引都是从 0 开始
 */
+
+const bool show = true;					// 是否在每次操作后显示相关信息
 
 typedef struct Node
 {
@@ -18,60 +24,85 @@ typedef struct Node
 	Node(int v = 0, Node* p = nullptr) : data(v), next(p) {};
 } Node;
 
-
 class List
 {
 private:
-	Node *head;
+	Node *head = nullptr;
+
 public:
 	List();
+	List(vector<int>& vec);				// 使用 vector 初始化链表 
 	~List();
 	void push_back(int value);			// 链表尾部添加元素
-	void insert(int value, int pos);	// 在 pos 位置插入数据
+	void insert(int value, int pos);	// 在 pos 位置插入数据 value
 	void deleteNode(int pos);			// 删除指定位置的结点
 	void clear();						// 清除链表（只留下头节点）
 	int length();						// 获取链表的长度
+	int getElem(int i);					// 获取链表中索引为 i 的节点的数据
 	void traverseList();				// 遍历链表并打印
 	Node* gethead() { return head; }	// 获取头节点
 };
+
+
 List::List()
 {
 	head = new Node(0);
+
+	if (show) traverseList();
+}
+
+List::List(vector<int>& vec)
+{
+	if (show) cout << "使用 vector 初始化 list" << endl;
+
+	head = new Node(0);
+	for (int i = 0; i < vec.size(); ++i)
+	{
+		this->push_back((vec)[i]);
+	}
+
+	if (show) traverseList();
 }
 
 List::~List()
 {
-	;
+	if (show) cout << "call ~List()" << endl; 
+	this->clear();						// delete[] 掉除头结点的结点
+	delete[] head;
+	head = nullptr;
 }
 
 void List::push_back(int value)
 {
-	// printf("push_bach(%d)\n", value);
+	if (show) printf("call push_back(%d)\n", value);
+	
 	Node *node = new Node(value);
-	Node *p = this->head;
+	Node *p = head;
 	while (p->next)
 	{
 		p = p->next;
 	}
 	p->next = node;
-	traverseList();
+	
+	if (show) traverseList();
 }
 
 void List::insert(int pos, int value)
 {
-	printf("insert(%d, %d)\n", pos, value);
+	if (show) printf("call insert(%d, %d)\n", pos, value);
+
 	if (pos < 0)
 	{
 		cout << "插入失败，插入的位置应该大于等于零" << endl;
 		return;
 	}
-	else if (pos >= length())
+	else if (pos >= length())		// 如果插入的位置比链表长度都大，直接插入在链表尾部
 	{
 		push_back(value);
 		return;
 	}
 
-	Node* p = head;
+	Node *p = head;
 	int index = 0;	// 第一个结点的索引为 0
 	while (p->next)
 	{
@@ -83,20 +114,23 @@ void List::insert(int pos, int value)
 			break;
 		}
 		p = p->next;
-		index++;
+		++index;
 	}
-	traverseList();
+	
+	if (show) traverseList();
 }
 
 void List::deleteNode(int pos)  // pos 索引
 {
-	// 更好的方法可以参考 mainJZ18.cpp
-	printf("deleteNode(%d)\n", pos);
+	// mainJZ18.cpp 提供了接口为 void deleteNode(ListNode* pListHead, ListNode* pToBeDeleted) 的实现
+	if (show) printf("call deleteNode(%d)\n", pos);
+
 	if (pos < 0 || pos >= length())
 	{
 		cout << "待删数据不在链表中" << endl;
 		return;
 	}
+
 	Node *p = head;
 	Node *pnext = p->next;
 	int cnt = 0;
@@ -111,15 +145,17 @@ void List::deleteNode(int pos)  // pos 索引
 		}
 		p = pnext;
 		pnext = pnext->next;
-		cnt++;
+		++cnt;
 	}
-	traverseList();
+	
+	if (show) traverseList();
 }
 
 void List::clear()
 {
-	cout << "clear()" << endl;
-	Node* p = head->next;
+	if (show) cout << "call clear()" << endl;
+
+	Node *p = head->next;
 	Node *pNode = nullptr;
 	while (p)
 	{
@@ -129,20 +165,46 @@ void List::clear()
 		pNode = nullptr;
 	}
 	head->next = nullptr;
-	traverseList();
+	
+	if (show) traverseList();
 }
 
 int List::length()
 {
 	int cnt = 0;
-	Node* p = head->next;
+	Node *p = head->next;
 	while (p)
 	{
 		cnt++;
 		p = p->next;
 	}
-	// cout << "length:" << cnt << endl;
+	cout << "length: " << cnt << endl;
 	return cnt;
+}
+
+int List::getElem(int pos)		// 索引
+{
+	if (show) printf("call getElem(%d)", pos);
+	
+	Node *p = head->next;
+	int cur = 0;
+	while (p && cur < pos)
+	{
+		p = p->next;
+		++cur;
+	}
+	
+	if (p)
+	{
+		if (show) printf(": %d\n", p->data);
+		return p->data;
+	}
+	else
+	{
+		printf("\ngetElem(%d) failed!\n", pos);
+		exit(-1);
+	}
+	
 }
 
 void List::traverseList()
@@ -156,24 +218,24 @@ void List::traverseList()
 	cout << "nullptr" << endl;
 }
 
-
 int main()
 {
-	List mylist;
-	mylist.push_back(7);
+	vector<int> vec {11, 55, 99};
+	List mylist(vec);
 	mylist.push_back(9);
 	mylist.insert(-1, -3);
 	mylist.insert(1, -1);
 	mylist.insert(0, 99);
-	mylist.insert(100, 4);
+	mylist.insert(100, 5);
+	mylist.getElem(3);
+	mylist.getElem(0);
+	// mylist.getElem(100);
+	
 	mylist.deleteNode(0);
 	mylist.deleteNode(2);
 	mylist.deleteNode(2);
 	mylist.deleteNode(2);
 	mylist.push_back(12);
-	mylist.clear();
 
-
-
-	system("pause");
+	// 退出 main 后执行析构函数
 }
